a537593bac724c5239d9c37e2b5f74e668245cfe
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-30 07:07:48 -0300

    Feat: Creating snake

---
+++ main.py
@@ -0,0 +1,26 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
